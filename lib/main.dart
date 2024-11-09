0e8debf66055d57b7e68f4291d76099d82f41c3f
Mhmod <mahmoud97787@gmail.com>
2024-11-09 17:48:55 +0200

    Launching the project.

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Docdoc());
+}
+
+class Docdoc extends StatelessWidget {
+  const Docdoc({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello World'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
