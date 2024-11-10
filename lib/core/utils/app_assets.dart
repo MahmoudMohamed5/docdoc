e14d4c8413f944c159e200409a1533e1526e3e68
Mhmod <mahmoud97787@gmail.com>
2024-11-10 12:48:38 +0200

    add Assets and AppAssets class

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,169 @@
+// ignore_for_file: prefer_single_quotes
+class AppAssets {
+  AppAssets._();
+  
+  /// Assets for imagesAndroid12splash
+  /// assets/images/android12splash.png
+  static const String imagesAndroid12splash = "assets/images/android12splash.png";
+
+  /// Assets for imagesCardBackground
+  /// assets/images/card_background.png
+  static const String imagesCardBackground = "assets/images/card_background.png";
+
+  /// Assets for imagesCardiologist
+  /// assets/images/cardiologist.png
+  static const String imagesCardiologist = "assets/images/cardiologist.png";
+
+  /// Assets for imagesDentistry
+  /// assets/images/dentistry.png
+  static const String imagesDentistry = "assets/images/dentistry.png";
+
+  /// Assets for imagesEnt
+  /// assets/images/ent.png
+  static const String imagesEnt = "assets/images/ent.png";
+
+  /// Assets for imagesFaceId
+  /// assets/images/face_id.png
+  static const String imagesFaceId = "assets/images/face_id.png";
+
+  /// Assets for imagesFlagOfEgypt
+  /// assets/images/flag_of_egypt.png
+  static const String imagesFlagOfEgypt = "assets/images/flag_of_egypt.png";
+
+  /// Assets for imagesGeneral
+  /// assets/images/general.png
+  static const String imagesGeneral = "assets/images/general.png";
+
+  /// Assets for imagesHepatology
+  /// assets/images/hepatology.png
+  static const String imagesHepatology = "assets/images/hepatology.png";
+
+  /// Assets for imagesHistologist
+  /// assets/images/histologist.png
+  static const String imagesHistologist = "assets/images/histologist.png";
+
+  /// Assets for imagesHomeDoctorInfo
+  /// assets/images/home_doctor_info.png
+  static const String imagesHomeDoctorInfo = "assets/images/home_doctor_info.png";
+
+  /// Assets for imagesImage1460
+  /// assets/images/image1460.png
+  static const String imagesImage1460 = "assets/images/image1460.png";
+
+  /// Assets for imagesIntestine
+  /// assets/images/intestine.png
+  static const String imagesIntestine = "assets/images/intestine.png";
+
+  /// Assets for imagesLogo
+  /// assets/images/logo.png
+  static const String imagesLogo = "assets/images/logo.png";
+
+  /// Assets for imagesNeurologic
+  /// assets/images/neurologic.png
+  static const String imagesNeurologic = "assets/images/neurologic.png";
+
+  /// Assets for imagesOnboardingDoctor
+  /// assets/images/onboarding_doctor.png
+  static const String imagesOnboardingDoctor = "assets/images/onboarding_doctor.png";
+
+  /// Assets for imagesOptometry
+  /// assets/images/optometry.png
+  static const String imagesOptometry = "assets/images/optometry.png";
+
+  /// Assets for imagesPediatric
+  /// assets/images/pediatric.png
+  static const String imagesPediatric = "assets/images/pediatric.png";
+
+  /// Assets for imagesPlaystore
+  /// assets/images/playstore.png
+  static const String imagesPlaystore = "assets/images/playstore.png";
+
+  /// Assets for imagesProfile
+  /// assets/images/profile.png
+  static const String imagesProfile = "assets/images/profile.png";
+
+  /// Assets for imagesPulmonary
+  /// assets/images/pulmonary.png
+  static const String imagesPulmonary = "assets/images/pulmonary.png";
+
+  /// Assets for imagesRadiology
+  /// assets/images/radiology.png
+  static const String imagesRadiology = "assets/images/radiology.png";
+
+  /// Assets for imagesRandyWigham
+  /// assets/images/randy_wigham.png
+  static const String imagesRandyWigham = "assets/images/randy_wigham.png";
+
+  /// Assets for imagesSplashScreen
+  /// assets/images/splash_screen.png
+  static const String imagesSplashScreen = "assets/images/splash_screen.png";
+
+  /// Assets for svgApple
+  /// assets/svg/apple.svg
+  static const String svgApple = "assets/svg/apple.svg";
+
+  /// Assets for svgBackArrow
+  /// assets/svg/back_arrow.svg
+  static const String svgBackArrow = "assets/svg/back_arrow.svg";
+
+  /// Assets for svgCalendar
+  /// assets/svg/calendar.svg
+  static const String svgCalendar = "assets/svg/calendar.svg";
+
+  /// Assets for svgCalendar2
+  /// assets/svg/calendar2.svg
+  static const String svgCalendar2 = "assets/svg/calendar2.svg";
+
+  /// Assets for svgCalendarTick
+  /// assets/svg/calendar_tick.svg
+  static const String svgCalendarTick = "assets/svg/calendar_tick.svg";
+
+  /// Assets for svgChangeProfileIcon
+  /// assets/svg/change_profile_icon.svg
+  static const String svgChangeProfileIcon = "assets/svg/change_profile_icon.svg";
+
+  /// Assets for svgFacebook
+  /// assets/svg/facebook.svg
+  static const String svgFacebook = "assets/svg/facebook.svg";
+
+  /// Assets for svgGoogle
+  /// assets/svg/google.svg
+  static const String svgGoogle = "assets/svg/google.svg";
+
+  /// Assets for svgHome
+  /// assets/svg/home.svg
+  static const String svgHome = "assets/svg/home.svg";
+
+  /// Assets for svgImage1460
+  /// assets/svg/image1460.svg
+  static const String svgImage1460 = "assets/svg/image1460.svg";
+
+  /// Assets for svgLogo
+  /// assets/svg/logo.svg
+  static const String svgLogo = "assets/svg/logo.svg";
+
+  /// Assets for svgLogoWithOpacity
+  /// assets/svg/logo_with_opacity.svg
+  static const String svgLogoWithOpacity = "assets/svg/logo_with_opacity.svg";
+
+  /// Assets for svgMessageText
+  /// assets/svg/message_text.svg
+  static const String svgMessageText = "assets/svg/message_text.svg";
+
+  /// Assets for svgNotification
+  /// assets/svg/notification.svg
+  static const String svgNotification = "assets/svg/notification.svg";
+
+  /// Assets for svgSearchNormal
+  /// assets/svg/search_normal.svg
+  static const String svgSearchNormal = "assets/svg/search_normal.svg";
+
+  /// Assets for svgVideo
+  /// assets/svg/video.svg
+  static const String svgVideo = "assets/svg/video.svg";
+
+  /// Assets for svgWallet2
+  /// assets/svg/wallet2.svg
+  static const String svgWallet2 = "assets/svg/wallet2.svg";
+}
+
