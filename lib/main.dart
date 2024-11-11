ff0331eb737ae3004fa31af92b7d3bfbbdf02265
Mhmod <mahmoud97787@gmail.com>
2024-11-11 05:16:03 +0200

    change name of app and ic_launcher

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:docdoc/core/routing/app_router.dart';
 import 'package:docdoc/doc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 // div branch
-void main() {
+void main() async {
+  await ScreenUtil.ensureScreenSize();
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
